57ba538f49641323977d12d7d44d1dfe62092717
Kevin Dam <kevindam@outlook.com>
2021-07-05 17:07:48 -0700

    Complete phonebook step6 and data for countries steps 1-3 (exercises 2.11-2.14)

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,5 +1,6 @@
-import './App.css';
-import React, { useState } from 'react'
+import './App.css'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = ({person}) => {
   return (
@@ -27,6 +28,51 @@ const PersonList = ({persons, filter}) => {
   }
 }
 
+const CountryList = ({countries, viewHandler, weatherHandler, weather}) => {
+  if (countries.length > 10) {
+    return (
+      <div>Too many matches, please further specify your search</div>
+    )
+  } else if (countries.length === 1) {
+    let country = countries[0]
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${country.capital}`)
+      .then(response => {
+        console.log(response.data)
+        weatherHandler(response.data)
+      })
+      return (
+        <div>
+          <h2>{country.name}</h2>
+          <br></br>
+          Capital: {country.capital}
+          <br></br>
+          Population: {country.population}
+          <h3>Languages:</h3>
+          <ul>
+            {country.languages.map(language => <li>{language.name}</li>)}
+          </ul>
+          <img src={country.flag} alt='' width="600" height="300"></img>
+
+          <div>
+            <h3>Weather in {country.capital}</h3>
+            <b>Temperature: </b>{weather.current.temperature} Celsius
+            <img src={weather.current.weather_icons[0]} alt="" />
+            <b>Wind: </b>{weather.current.wind_speed} direction {weather.current.wind_dir}
+          </div>
+        </div>
+      )
+
+  } else {
+    return (
+      <div>
+        {countries.map(country => <div>{country.name}
+        <button onClick={viewHandler} value={country.name}>Show</button></div>)}
+      </div>
+    )
+  }
+}
+
 const Filter = ({value, onChange}) => {
   return (
     <div>
@@ -39,16 +85,40 @@ const Filter = ({value, onChange}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNum, setNewNum ] = useState('')
   const [ newFilter, setNewFilter ] = useState('')
+  const [ newCountryFilter, setNewCountryFilter ] = useState('')
+  const [ countries, setCountries ] = useState([])
+  const [ weather, setWeather ] = useState({
+    "current": {
+      "temperature": 0,
+      "weather_icons": [""],
+      "wind_speed": 0,
+      "wind_dir": ""
+    }
+  })
+
+  const personsHook = () => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      console.log('Persons Promise Fulfilled')
+      setPersons(response.data)
+    })
+  }
+
+  useEffect(personsHook, [])
+
+  const countriesHook = () => {
+    axios.get(`https://restcountries.eu/rest/v2/name/${newCountryFilter}`)
+    .then(response => {
+      console.log('Countries Promise Fulfilled')
+      console.log(response.data)
+      setCountries(response.data)
+    })
+  }
+
+  useEffect(countriesHook)
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
@@ -60,6 +130,18 @@ const App = () => {
 
   const handleFilterChange = (e) => {
     setNewFilter(e.target.value)
+  }
+
+  const handleCountryFilterChange = (e) => {
+    setNewCountryFilter(e.target.value)
+  }
+
+  const handleShowCountryView = (e) => {
+    setNewCountryFilter(e.target.value)
+  }
+
+  const handleWeather = (e) => {
+    setWeather(e)
   }
 
   const addPerson = (e) => {
@@ -96,6 +178,13 @@ const App = () => {
       <Filter value={newFilter} onChange={handleFilterChange} />
       <h2>Numbers</h2>
       <PersonList persons={persons} filter={newFilter} />
+
+      <br></br>
+
+      <div>Find Countries: <input value={newCountryFilter} onChange={handleCountryFilterChange} /></div>
+      <div>
+        <CountryList countries={countries} viewHandler={handleShowCountryView} weatherHandler={handleWeather} weather={weather}/>
+      </div>
     </div>
   )
 }
