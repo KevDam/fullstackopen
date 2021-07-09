db59bc4df7a3a919661c5938ca6c019f32a1f96b
Kevin Dam <kevindam@outlook.com>
2021-07-09 13:00:30 -0700

    Clean up phonebook/src/App.js

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -34,50 +34,6 @@ const PersonList = ({persons, filter, handleDeletePerson}) => {
   }
 }
 
-// const CountryList = ({countries, viewHandler, weatherHandler, weather}) => {
-//   if (countries.length > 10) {
-//     return (
-//       <div>Too many matches, please further specify your search</div>
-//     )
-//   } else if (countries.length === 1) {
-//     let country = countries[0]
-//     axios
-//       .get(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${country.capital}`)
-//       .then(response => {
-//         console.log(response.data)
-//         weatherHandler(response.data)
-//       })
-//       return (
-//         <div>
-//           <h2>{country.name}</h2>
-//           <br></br>
-//           Capital: {country.capital}
-//           <br></br>
-//           Population: {country.population}
-//           <h3>Languages:</h3>
-//           <ul>
-//             {country.languages.map(language => <li>{language.name}</li>)}
-//           </ul>
-//           <img src={country.flag} alt='' width="600" height="300"></img>
-
-//           <div>
-//             <h3>Weather in {country.capital}</h3>
-//             <b>Temperature: </b>{weather.current.temperature} Celsius
-//             <img src={weather.current.weather_icons[0]} alt="" />
-//             <b>Wind: </b>{weather.current.wind_speed} direction {weather.current.wind_dir}
-//           </div>
-//         </div>
-//       )
-
-//   } else {
-//     return (
-//       <div>
-//         {countries.map(country => <div>{country.name}
-//         <button onClick={viewHandler} value={country.name}>Show</button></div>)}
-//       </div>
-//     )
-//   }
-// }
 
 const Filter = ({value, onChange}) => {
   return (
@@ -108,16 +64,6 @@ const App = () => {
   const [ newNum, setNewNum ] = useState('')
   const [ newFilter, setNewFilter ] = useState('')
   const [ newError, setnewError ] = useState('')
-  // const [ newCountryFilter, setNewCountryFilter ] = useState('')
-  // const [ countries, setCountries ] = useState([])
-  // const [ weather, setWeather ] = useState({
-  //   "current": {
-  //     "temperature": 0,
-  //     "weather_icons": [""],
-  //     "wind_speed": 0,
-  //     "wind_dir": ""
-  //   }
-  // })
 
   const personsHook = () => {
     personService.getAll().then(response => {
@@ -127,17 +73,6 @@ const App = () => {
   }
 
   useEffect(personsHook, [])
-
-  // const countriesHook = () => {
-  //   axios.get(`https://restcountries.eu/rest/v2/name/${newCountryFilter}`)
-  //   .then(response => {
-  //     console.log('Countries Promise Fulfilled')
-  //     console.log(response.data)
-  //     setCountries(response.data)
-  //   })
-  // }
-
-  // useEffect(countriesHook)
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
@@ -160,23 +95,10 @@ const App = () => {
 
   }
 
-  // const handleCountryFilterChange = (e) => {
-  //   setNewCountryFilter(e.target.value)
-  // }
-
-  // const handleShowCountryView = (e) => {
-  //   setNewCountryFilter(e.target.value)
-  // }
-
-  // const handleWeather = (e) => {
-  //   setWeather(e)
-  // }
-
   const addPerson = (e) => {
     e.preventDefault()
 
     if (persons.find(person => (person.name === newName && person.number === newNum))) {
-      // window.alert(`${newName} is already added to the phonebook`)
       setnewError(`${newName} is already added to the phonebook`)
     } else if (persons.find(person => person.name === newName)) {
       let selectedPerson = persons.find(person => person.name === newName)
@@ -234,14 +156,7 @@ const App = () => {
       </form>
       <Filter value={newFilter} onChange={handleFilterChange} />
       <h2>Numbers</h2>
-      <PersonList persons={persons} filter={newFilter} handleDeletePerson={handleDeletePerson}/>
-
-      <br></br>
-
-      {/* <div>Find Countries: <input value={newCountryFilter} onChange={handleCountryFilterChange} /></div>
-      <div>
-        <CountryList countries={countries} viewHandler={handleShowCountryView} weatherHandler={handleWeather} weather={weather}/>
-      </div> */}
+      <PersonList persons={persons} filter={newFilter} handleDeletePerson={handleDeletePerson}/>∂
     </div>
   )
 }
